c86010362e2477dbad3d67c0c81e5ffb52b494aa
xuekui <xk.136@qq.com>
2020-06-03 13:30:10 +0800

    fix fake api server run info

--- pkg/apiserver/internal/apiserver.go
+++ pkg/apiserver/internal/apiserver.go
@@ -13,14 +13,15 @@ import (
 // APIServerDefaultArgs allow tests to run offline, by preventing API server from attempting to
 // use default route to determine its --advertise-address.
 var APIServerDefaultArgs = []string{
-	"--advertise-address=127.0.0.1",
+	"--advertise-address=0.0.0.0",
+	"--bind-address=0.0.0.0",
 	"--etcd-servers={{ if .EtcdURL }}{{ .EtcdURL.String }}{{ end }}",
 	"--cert-dir={{ .CertDir }}",
 	"--insecure-port={{ if .URL }}{{ .URL.Port }}{{ end }}",
 	"--insecure-bind-address={{ if .URL }}{{ .URL.Hostname }}{{ end }}",
 	"--secure-port={{ if .SecurePort }}{{ .SecurePort }}{{ end }}",
 	"--disable-admission-plugins=ServiceAccount",
-	"--service-cluster-ip-range=10.0.0.0/24",
+	"--service-cluster-ip-range=10.10.0.0/24",
 	"--allow-privileged=true",
 }
 
